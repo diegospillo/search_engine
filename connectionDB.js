8085d795fb020c173056ebf6233419a0b567e044
diegospillo <diego12juve@hotmail.com>
2023-11-03 15:21:06 +0100

    Crazione tabbella classi

--- connectionDB.js
+++ connectionDB.js
@@ -1,12 +1,18 @@
-const { Client } = require('pg')
-const client = new Client({
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
   user: 'diegospillo',
   host: 'dpg-ckvnb8i37rbc739en73g-a',
   database: 'pizzadb_tquo',
   password: 'v29fHJ386OPDLE4bcQLcqkjxBjBuUBt1',
   port: 5432,
-})
-client.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+});
+
+pool.query("CREATE TABLE Classi (id SERIAL PRIMARY KEY, anno VARCHAR(1) NOT NULL, sezione VARCHAR(2) NOT NULL)", (err, result) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Tabella creata con successo!");
+  }
 });
