c5047a359a9b21e2b7a3417e2041f28deb1edc1b
diegospillo <diego12juve@hotmail.com>
2023-11-06 18:29:21 +0100

    ordini 4

--- query/Ordini.js
+++ query/Ordini.js
@@ -70,7 +70,8 @@ function Get_ordini_classe(req, res) {
         } else {//
           const studenti_classe = result1.rows;
           const id_studenti_classe = studenti_classe.map(studente => studente.id);
-          console.log("Dati letti con successo!");
+          console.log(id_studenti_classe);
+          console.log("Dati letti con successo!");//VEDERE ORDINI CLASSE!!!!!!!!!!!
           pool.query(`SELECT * FROM Ordini WHERE id_studente IN ('${id_studenti_classe}');`, (err, result2) => {
             if (err) {
               console.error(err);
