b836a054ef66d787ffbdd33cd751758670d1df0d
diegospillo <diego12juve@hotmail.com>
2023-11-14 13:12:31 +0100

    fixed3 2

--- request_axios/request.js
+++ request_axios/request.js
@@ -1,22 +1,24 @@
 const axios = require('axios');
 
+const url = "https://pizzappbackend.onrender.com";
+
 async function getClassi() {
-    const response = await axios.get('/get_Classi');
+    const response = await axios.get(url+'/get_Classi');
     return response;
 }
 
 async function getPizze() {
-    const response = await axios.get('/get_Pizze');
+    const response = await axios.get(url+'/get_Pizze');
     return response;
 }
 
 async function getStudenti() {
-    const response = await axios.get('/get_Studenti');
+    const response = await axios.get(url+'/get_Studenti');
     return response;
 }
 
 async function getOrdini() {
-    const response = await axios.get('/get_Ordini');
+    const response = await axios.get(url+'/get_Ordini');
     return response;
 }
 
